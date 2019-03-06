9fe55e3925c34b7c2b69788abd97078f19e18c18
andrew-d-gordon <47040038+andrew-d-gordon@users.noreply.github.com>
2019-03-05 23:16:42 -0800

    Add files via upload

--- huffman.py
+++ huffman.py
@@ -1,7 +1,6 @@
 '''
 Created on Feb 24, 2019
-Work in Progress, can successfully encode and decode files.
-Needs encoded to be transferred to bit representation of encoding and traversal in order to compress.
+
 @author: Gordon
 '''
 import sys
@@ -13,11 +12,14 @@ class Node:
         self.lc = None
         self.key = key
    
-'''To encode: Make a small tree first with root node.
-     Make a new root node and then set that to 
-     be the head of the smaller bst, and add 
-     left child to be next biggest probability. Continue
-     this process til all characters have been accounted for.'''
+'''To encode: Make list of most frequent nodes, build huffman tree by
+taking two smallest and making them leaves of a root node that is null.
+Next make a new root node where you add the tree you just made as the right
+child, and the next most likely char as the left child. Do this until
+tree is complete. Then go through tree encoding your actual message with
+moves to the left as 0s, and moves through the tree to the right as 1s. Next
+encode your sequence of 1s and 0s as bits, convert every 8 into an ascii
+char which helps to compress, add postorder traversal and leftover 1s and 0s, to the end.'''
                     
 def encode(message):
     '''if all characters are possible, rework array to be in range of all possible ascii characters'''
@@ -38,7 +40,7 @@ def encode(message):
     for i in range(len(frequencyarray)):
         if frequencyarray[i][0] != 0:
             charlist+=frequencyarray[i][0]
-    print(charlist, "<--charlist")
+    #print(charlist, "<--charlist")
     
     '''now we will build tree adding smaller trees of probability,
      starting from leftmost end in charlist'''
@@ -70,46 +72,101 @@ def encode(message):
     #print(realroot.key, "<-- bst root key,", realroot.rc.key, "<-- should be none", realroot.lc.key, "<-- should not be none")
     #print(realroot.rc.lc.key, "<-- should not be none")
     '''Now that we have our tree, we have to construct encoded message'''
+    '''add 1s and 0s to bitstring until it is of length 8 (i.e. bitcount is 8),
+     then add chr(int(string, base=2)) to encoded'''
     encodedmessage = ""
-    
+    bitstring = ''
+    bitcount=0
+    asciicharcount = 0
     for i in enumerate(message):
         found = False
         findernode = realroot
+        #print("bitcount at top:", bitcount)
+        #print("bitstring at top:", bitstring)
+        if bitcount == 8:
+            #print("adding bitstring")
+            encodedmessage+=chr(int(bitstring, base=2))
+            bitcount = 0
+            bitstring = ''
+            asciicharcount+=1
         while(found!=True):
             if findernode.lc.key == i[1]:
-                encodedmessage+=str(0)
+                bitstring+='0'
+                bitcount+=1
+                #print("bitcount:", bitcount)
+                #print("bitstring in left child:", bitstring)
                 found = True
+                if bitcount == 8:
+                    #print("adding bitstring:", bitstring)
+                    encodedmessage+=chr(int(bitstring, base=2))
+                    bitcount = 0
+                    bitstring = ''
+                    asciicharcount+=1
             elif findernode.rc.key == i[1]:
-                encodedmessage+=str(1)
+                bitstring+='1'
+                bitcount+=1
+                #print("bitcount:", bitcount)
+                #print("bitstring in right child:", bitstring)
                 found = True
+                if bitcount == 8:
+                    #print("adding bitstring:", bitstring)
+                    encodedmessage+=chr(int(bitstring, base=2))
+                    bitcount = 0
+                    bitstring = ''
+                    asciicharcount+=1 
             else:
-                encodedmessage+=str(1)
+                bitstring+='1'
                 findernode = findernode.rc
+                bitcount+=1
+                #print("bitcount:", bitcount)
+                #print("bitstring so far:", bitstring)
+                if bitcount == 8:
+                    #print("adding bitstring:", bitstring)
+                    encodedmessage+=chr(int(bitstring, base=2))
+                    bitcount = 0
+                    bitstring = ''
+                    asciicharcount+=1
+    #print(encodedmessage, "this is encodedmessage with charlist")  
+    #print("done adding ascii characters to message, there are:", asciicharcount, "characters")
+    #print(encodedmessage, "this is encodedmessage with charlist")          
+    if bitcount!=0:
+        #left over bitstring added to the end
+        encodedmessage+=bitstring
+    #add space between message and  charlist (post order trav.)          
     encodedmessage+=' '
     encodedmessage+=charlist
-    #print(' '.join(format(ord(x), 'b') for x in charlist))
-    #map(bin, bytearray(encodedmessage))
-    #st = "hello world"
-    #' '.join(format(x, 'b') for x in bytearray(st))
-    #print(st)
-    #print(encodedmessage)
-    #print(encodedmessage)
-    #print("{0:b}".format(10))
-    #print(encodedmessage)
     return(encodedmessage)
 
     
-'''To decode: take in binary message, if 0, move to left child,
-if 1, move to right child, once you have found a character, reset
-finder node to be the root of your bst again, then continue process
-until message has been completely decoded.'''
-def decode(encodedmessage):
-    '''want to pull postorder from encodedmessage then build tree, then move 
-    through our tree from tree root, by going to the left child when we see 
-    a 0, and going to a right child when we see a 1 until we find a letter,
-    then we start back from the top repeat until we have found all of the letters'''
-    
-    '''pull out postorder traversal from encodedmessage'''
+'''To decode: take in ascii encoded message, make it binary (with postorder
+ traversal at end, rebuild tree from postorder, then rebuild message and
+ return it.'''
+def decode(encodedmessage):  
+    encoded = ""
+    messageend = 0
+    done = False
+    '''recovering message from mostly masked representation'''
+    for x in range(len(encodedmessage)):
+        if encodedmessage[x]==' ':
+            break
+        elif encodedmessage[x] == '1':
+            encoded+='1'
+            messageend+=1
+        elif encodedmessage[x] == '0':
+            encoded+='0'
+            messageend+=1
+        else:
+            #print("ord(x[1])):", ord(encodedmessage[x]))
+            oldtxt = bin(ord(encodedmessage[x]))
+            for i in range(0,8-len(oldtxt[2:])):
+                encoded+='0'
+            encoded+=oldtxt[2:]
+            messageend+=1
+                
+    encoded+=encodedmessage[messageend:]
+    #print(encoded)
+
+    '''pull out postorder traversal from encoded'''
     postordertraversal = ""
     postorderstartfound = False
     i=0
@@ -117,12 +174,12 @@ def decode(encodedmessage):
     while postorderstartfound==False:
         #looks for last entry of encodedmessage (postorder stored at end of passedmessage)
         endofencoded+=1
-        if encodedmessage[i] == ' ':
+        if encoded[i] == ' ':
             #print("found space")
             postorderstartfound=True
         #print(encodedmessage[i])
         i=i+1
-    postordertraversal = encodedmessage[endofencoded:]
+    postordertraversal = encoded[endofencoded:]
     #print(postordertraversal, "<--postordertraversal in decode")
     #print(encodedmessage[endofencoded])
     #print(len(postordertraversal)+2)
@@ -166,12 +223,12 @@ def decode(encodedmessage):
         #print(i[1])
         #print(findernode.lc.key)
         #print(encodedmessage[i])
-        if encodedmessage[i] == ("1"):
+        if encoded[i] == ("1"):
             findernode = findernode.rc
             if findernode.key != None:
                 decodedmessage+=findernode.key
                 findernode = realroot
-        elif encodedmessage[i] == '0':
+        elif encoded[i] == '0':
             findernode=findernode.lc
             decodedmessage+=findernode.key
             findernode=realroot
@@ -179,14 +236,17 @@ def decode(encodedmessage):
     return(decodedmessage)
 
 if __name__ == '__main__':
-    '''#globals()[sys.argv[1]]
+    #globals()[sys.argv[1]]
     script_name = sys.argv[0]
     fi = open(sys.argv[2], "r")
     fidata = fi.read().replace('\n', '')
-    sys.argv[3].write(globals()[sys.argv[1]](fidata))'''
+    fo = open(sys.argv[3], "w")
+    fo.write(globals()[sys.argv[1]](fidata))
     pass
-    
+
+#test input   
+'''#message = "heyo let us get this bread brethren 9%#wE6t"
 testmessage="a b c d e f g h i j k l m n o p... :;][\.,o129385089uoiquwet$^&*&^%$#@!"
 encodedmessage = encode(testmessage)
-print("encoded message:", encodedmessage)
-print("decoded message:", decode(encodedmessage))
+#("encoded message:", encodedmessage)
+print("decoded message:", decode(encodedmessage))'''
