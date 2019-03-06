1d8cc05c270779312e6240d13abb5d0615f87ad5
andrew-d-gordon <47040038+andrew-d-gordon@users.noreply.github.com>
2019-03-05 23:18:46 -0800

    Update huffman.py

--- huffman.py
+++ huffman.py
@@ -245,7 +245,7 @@ if __name__ == '__main__':
     pass
 
 #test input   
-testmessage="a b c d e f g h i j k l m n o p... :;][\.,o129385089uoiquwet$^&*&^%$#@!"
+'''testmessage="a b c d e f g h i j k l m n o p... :;][\.,o129385089uoiquwet$^&*&^%$#@!"
 encodedmessage = encode(testmessage)
 #("encoded message:", encodedmessage)
 print("decoded message:", decode(encodedmessage))'''
